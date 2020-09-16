f0ff1b7a9d2a7e9b93fe46e958e9398a94e24f37
Zoriana <makieieva.dev@gmail.com>
2020-09-16 13:15:16 +0300

    Updates

---
+++ precache-manifest.bbb95a80727513e17ca2ec0c69a7bd21.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8481de9618fac0cf091e4f4fa389d98d",
+    "url": "/react-hw-03-image-finder/index.html"
+  },
+  {
+    "revision": "0461ef2877de6a6bbfc4",
+    "url": "/react-hw-03-image-finder/static/css/main.1fbf55dd.chunk.css"
+  },
+  {
+    "revision": "612334f05254eebc7029",
+    "url": "/react-hw-03-image-finder/static/js/2.d741cb04.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-hw-03-image-finder/static/js/2.d741cb04.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0461ef2877de6a6bbfc4",
+    "url": "/react-hw-03-image-finder/static/js/main.0575ca85.chunk.js"
+  },
+  {
+    "revision": "063253bfbd13644b51dd",
+    "url": "/react-hw-03-image-finder/static/js/runtime-main.f4f19f46.js"
+  }
+]);
